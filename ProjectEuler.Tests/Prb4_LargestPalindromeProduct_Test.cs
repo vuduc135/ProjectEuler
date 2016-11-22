9ddf15ef4b5e0c0087977fc890bb113bfb0d91b4
duc.vu@gmail.com <duc.vu@niteco.se>
2016-11-22 11:02:47 +0700

    Solve problem 4: Largest palindrome product

---
+++ ProjectEuler.Tests/Prb4_LargestPalindromeProduct_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Prb4_LargestPalindromeProduct_Test
+    {
+        [Test]
+        [TestCase(9009, true)]
+        [TestCase(9019, false)]
+        [TestCase(90109, true)]
+        public void IsPalindromeNumber_TestMethod(int number, bool expectedResult)
+        {
+            var lpd = new Prb4_LargestPalindromeProduct();
+            var result = lpd.IsPalindromeNumber(number);            
+            Assert.AreEqual(result,expectedResult, "Function returned a different value");
+        }
+
+        [Test]
+        [TestCase(1,99)]
+        [TestCase(2,9009)]
+        [TestCase(3, 9009)]
+        public void PalindromeNumbersList_TestMethod(int digit, int palindromeNumberExpected)
+        {
+            var lpd = new Prb4_LargestPalindromeProduct();
+            var result = lpd.PalindromeNumbersList(digit);
+            Assert.AreEqual(result, palindromeNumberExpected, "Function returned a different value");
+        }
+    }
+}
---
+++ ProjectEuler/Prb4_LargestPalindromeProduct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Problem 4: Largest palindrome product
+    /// https://projecteuler.net/problem=4
+    /// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    /// </summary>
+    public class Prb4_LargestPalindromeProduct
+    {
+        //Function for determining a number is palindrome number
+        public bool IsPalindromeNumber(int number)
+        {
+            var number2string = number.ToString();
+            var length = number2string.Length;
+                      
+            for (int i = 0; i < length / 2 ; i++)
+            {
+                if (number2string[i] != number2string[length - i - 1])
+                {
+                    return false;
+                }                
+            }
+            return true;                   
+        }
+
+        // Function for find palindrome number list
+        public int PalindromeNumbersList(int digitNumber)
+        {           
+            var minimum = Convert.ToInt32(Math.Pow(10, digitNumber - 1));
+            var maximum = Convert.ToInt32(Math.Pow(10, digitNumber) - 1);
+
+            var palindNumberList = Enumerable.Range(minimum, maximum - minimum + 1).ToArray();
+
+            var enumable = Enumerable.Range(minimum, maximum - minimum + 1)                             
+                             .SelectMany(x => palindNumberList.Select(y => x * y))
+                             .Where(IsPalindromeNumber);
+            return enumable.Max();
+        }
+        
+    }
+}
